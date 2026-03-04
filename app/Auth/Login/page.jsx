68d3b597f5d7627df28ccd6d02bf27507a175b50
nidinbose <nidinbose700@gmail.com>
2026-03-04 22:37:12 +0530

    asdffg

---
+++ app/Auth/Login/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Login(){
+  return (
+    <div className="h-screen flex items-center justify-center text-3xl container mx-auto">
+      <h1 className="leading-[1.7] bg-gradient-to-r from-[#FF902F] via-[#4C11CE] to-[#723CEB] bg-clip-text text-transparent">
+        Login
+      </h1>
+    </div>
+  )
+}
